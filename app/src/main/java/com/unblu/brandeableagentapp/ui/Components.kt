60eea00401c09e8b0f0c00fc052762344e6a0602
Hugo Gonçalves <hugo.goncalves@unblu.com>
2023-05-25 11:22:35 +0200

    UI improvement

--- app/src/main/java/com/unblu/brandeableagentapp/ui/Components.kt
+++ app/src/main/java/com/unblu/brandeableagentapp/ui/Components.kt
@@ -6,9 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import com.unblu.brandeableagentapp.R
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
@@ -17,8 +18,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.isSpecified
 import androidx.compose.ui.unit.sp
+import com.unblu.brandeableagentapp.R
 import com.unblu.brandeableagentapp.model.AuthenticationType
 
 @Composable
@@ -31,6 +32,7 @@ fun LabeledTextField(
     inputBackground: Color,
     borderColor: Color,
     inputTextColor: Color,
+    maxLines : Int = 1,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     trailingIcon: (@Composable () -> Unit)? = null
 ) {
@@ -44,23 +46,24 @@ fun LabeledTextField(
             text = label
         )
         Spacer(Modifier.width(4.dp))
-        TextField(
+        OutlinedTextField(
+            shape = RoundedCornerShape(4.dp),
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .height(inputHeight)
-                .background(color = inputBackground)
-                .border(1.dp, borderColor, shape = RoundedCornerShape(4.dp)),
+                .background(color = inputBackground),
             visualTransformation = visualTransformation,
             trailingIcon = trailingIcon,
+            textStyle = TextStyle(fontSize = (inputHeight.value*0.30).sp),
             maxLines = 1,
-            colors = TextFieldDefaults.textFieldColors(
+            colors = TextFieldDefaults.outlinedTextFieldColors(
                 textColor = inputTextColor,
                 cursorColor = inputTextColor,
                 backgroundColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
+                focusedBorderColor = borderColor,
+                unfocusedBorderColor = borderColor,
                 trailingIconColor = colorResource(id = R.color.login_input_text)
             )
         )
@@ -144,10 +147,11 @@ fun SettingsTextField(
         value = value,
         onValueChange = onValueChange,
         modifier = Modifier.fillMaxWidth(),
-        inputHeight = 48.dp,
+        inputHeight = 52.dp,
         inputBackground = Color.White,
         borderColor = Color.Gray,
-        inputTextColor = Color.Black
+        inputTextColor = Color.Black,
+        maxLines = 2,
     )
 }
 
